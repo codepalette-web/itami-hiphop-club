59a522c2c2381663ae0a9d2398d70deb49ed751c
Naoko705310 <naokos79@gmail.com>
2026-01-22 22:30:29 +0900

    ラジオボタンのどちらを選んでも、すべての項目が表示されるように修正

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,6 @@
 jQuery(function ($) {
   // この中であればWordpressでも「$」が使用可能になる
 
-  // お問い合わせフォームのラジオボタン切り替え
-  var $radioButtons = $('input[name="inquiry-type"]');
-  var $trialFields = $('.contact__form-item--trial');
+  // フォームのすべての項目を常に表示（切り替え機能は削除）
 
-  // 初期表示の設定
-  function updateFormFields() {
-    var selectedValue = $('input[name="inquiry-type"]:checked').val();
-    if (selectedValue === 'trial') {
-      $trialFields.show();
-    } else {
-      $trialFields.hide();
-    }
-  }
-
-  // ページ読み込み時に実行
-  updateFormFields();
-
-  // ラジオボタン変更時に実行
-  $radioButtons.on('change', updateFormFields);
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,6 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-  // お問い合わせフォームのラジオボタン切り替え
-  const $radioButtons = $('input[name="お問い合わせ種別"]');
-  const $trialFields = $('.contact__form-item--trial');
-
-  // 初期表示の設定
-  function updateFormFields() {
-    const selectedValue = $('input[name="お問い合わせ種別"]:checked').val();
-    if (selectedValue === '体験レッスン申し込み') {
-      $trialFields.show();
-    } else {
-      $trialFields.hide();
-    }
-  }
-
-  // ページ読み込み時に実行
-  updateFormFields();
-
-  // ラジオボタン変更時に実行
-  $radioButtons.on('change', updateFormFields);
+  // フォームのすべての項目を常に表示（切り替え機能は削除）
 
 });
