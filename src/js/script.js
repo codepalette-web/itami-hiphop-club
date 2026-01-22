f2319e08261fa2bc45be94a6f364ef0dd015ff75
Naoko705310 <naokos79@gmail.com>
2026-01-22 22:07:54 +0900

    name属性をふたたび日本語に変更

--- src/js/script.js
+++ src/js/script.js
@@ -2,13 +2,13 @@
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
   // お問い合わせフォームのラジオボタン切り替え
-  const $radioButtons = $('input[name="inquiry-type"]');
+  const $radioButtons = $('input[name="お問い合わせ種別"]');
   const $trialFields = $('.contact__form-item--trial');
 
   // 初期表示の設定
   function updateFormFields() {
-    const selectedValue = $('input[name="inquiry-type"]:checked').val();
-    if (selectedValue === 'trial') {
+    const selectedValue = $('input[name="お問い合わせ種別"]:checked').val();
+    if (selectedValue === '体験レッスン申し込み') {
       $trialFields.show();
     } else {
       $trialFields.hide();
